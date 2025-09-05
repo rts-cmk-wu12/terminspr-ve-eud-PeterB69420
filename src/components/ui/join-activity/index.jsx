55bd9615ec931fd6e39f61a74e4a0650c71e9690
Johndillerman <U2486892@rts-365.dk>
2025-09-05 14:40:32 +0200

    param fix

--- src/components/ui/join-activity/index.jsx
+++ src/components/ui/join-activity/index.jsx
@@ -2,18 +2,18 @@
 
 import { useRouter } from "next/navigation";
 
-export default function JoinActivityButton({ activityId, token, userId }) {
+export default function JoinActivityButton({ activityId, authToken, userId }) {
     const router = useRouter();
 
     async function joinActivity() {
-        if (!token) {
+        if (!authToken) {
             router.push("/login");
             return;
         }
         await fetch(`http://localhost:4000/api/v1/users/${userId}/activities/${activityId}`, {
             method: "POST",
             headers: {
-                "Authorization": "Bearer " + token
+                "Authorization": "Bearer " + authToken
             }
         });
         
